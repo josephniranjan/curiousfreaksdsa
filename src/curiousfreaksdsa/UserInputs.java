5dfd00e0f3ea95e6dfbb7fabeee19ed70bb99359
galdine <galdinejernik@gmail.com>
2025-08-17 20:54:07 +0530

    User Input

---
+++ src/curiousfreaksdsa/UserInputs.java
@@ -0,0 +1,22 @@
+package curiousfreaksdsa;
+import java.util.*;
+
+public class UserInputs {
+	  /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+   
+  public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+    int age = s.nextInt();
+    char gender = s.next().charAt(0);
+    double weight = s.nextDouble();
+    s.nextLine();
+    String name = s.nextLine();
+    
+    System.out.println(age);
+    System.out.println(gender);
+    System.out.println(weight);
+    System.out.println(name);
+      
+    s.close();
+  }
+}
