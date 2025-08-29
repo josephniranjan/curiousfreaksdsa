714a32e8f4165bcf8f9a1597484a2523e92d4b42
josephniranjan <46956514+josephniranjan@users.noreply.github.com>
2025-08-29 11:03:56 +0530

    Update CalculateGrade.java

--- src/warmuppractice/problem1/CalculateGrade.java
+++ src/warmuppractice/problem1/CalculateGrade.java
@@ -6,13 +6,13 @@ class CalculateGrade {
         int mark = s.nextInt();
         if(mark >= 90){
           System.out.println("Grade A");
-        }else if(mark >= 80){
+        } else if(mark >= 80){
           System.out.println("Grade B");
-        }else if(mark >= 60){
+        } else if(mark >= 60){
           System.out.println("Grade C");
-        }else if(mark >= 35){
+        } else if(mark >= 35){
           System.out.println("Grade D");
-        }else if(mark < 35){
+        } else{
           System.out.println("Fail");
         }
     }
